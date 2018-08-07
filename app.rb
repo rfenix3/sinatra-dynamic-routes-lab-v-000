0fe24ed6d488f02d95c2ce0120dc487e166b5b1a
Richard Fenix <richard.fenix@gmail.com>
2018-08-07 19:14:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,10 +18,11 @@ class App < Sinatra::Base
   # accepts a number and a phrase and returns that phrase in a string the number of times given
   get '/say/:number/:phrase' do
     binding.pry
+    answer = ''
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times do |i|
-      "#{@phrase}"
+      answer += "#{@phrase}/n"
     end
   end
 
@@ -36,3 +37,25 @@ class App < Sinatra::Base
   end
 
 end
+
+# get '/say/:word1/:word2/:word3/:word4/:word5' do
+#     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+#   end
+#
+#   get '/:operation/:number1/:number2' do
+#     operation = params[:operation]
+#     number1 = params[:number1].to_i
+#     number2 = params[:number2].to_i
+#     if operation == "add"
+#       answer = number1 + number2
+#     elsif operation == "subtract"
+#       answer = number1 - number2
+#     elsif operation == "multiply"
+#       answer = number1 * number2
+#     elsif operation == "divide"
+#       answer = number1 / number2
+#     else
+#       answer = "Unable to perform this operation"
+#     end
+#     answer.to_s
+#   end
